459a1b7d4bd50d23d11ce487994853e2f783ef59
Omar Belaizi <obelaizi@e3r11p9.1337.ma>
2023-12-19 22:09:09 +0100

    Start Scaning Monsters

--- main.cpp
+++ main.cpp
@@ -7,27 +7,15 @@
 #include <set>
 
 using namespace std;
-map<int, pair<int, int>> fshs;
+set<int> fshs;
 int myX1, myX2, myY1, myY2, myID1, myID2;
-
+set<int> enemies;
 /**
  * Score points by scanning valuable fish faster than your opponent.
  **/
 int calcDist(int x1, int y1, int x2, int y2) {
     return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 }
-
-int getId() {
-    int mx = 1e9, id;
-    for (auto &i: fshs) {
-        if (calcDist(myX1, myY1, i.second.first, i.second.second) < mx) {
-            mx = calcDist(myX1, myY1, i.second.first, i.second.second);
-            id = i.first;
-        }
-    }
-    return id;
-}
-
 
 void displayMove(vector<string> &rdrs, int x, int y, int l) {
     int tr=0,tl=1,bl=2,br=3;
@@ -42,35 +30,36 @@ void displayMove(vector<string> &rdrs, int x, int y, int l) {
         if (i.first == "BR" && calcDist(x, y, 9999, 9999) < mn) mn = calcDist(x, y, 9999, 9999), check = 3;
     }
     if (fshs.empty())
-        cout << "MOVE "<< x << " 0 "<<l<<endl;
+        cout << "MOVE "<< x << " 0 "<< l << "\n";
     else if (check == tr)
-        cout << "MOVE 9999 0 "<<l<<endl;
+        cout << "MOVE 9999 0 "<<l<<"\n";
     else if (check == tl)
-        cout << "MOVE 0 0 "<<l<<endl;
+        cout << "MOVE 0 0 " << l << "\n";
     else if (check == bl)
-        cout << "MOVE 0 9999 "<<l<<endl;
+        cout << "MOVE 0 9999 " << l << "\n";
     else
-        cout << "MOVE 9999 9999 "<<l<<endl;
+        cout << "MOVE 9999 9999 " << l << "\n";
 }
 
 int main()
 {
+    set<int> fishesScaned1, fishesScaned2;
     int creature_count;
     cin >> creature_count; cin.ignore();
-    for (int i = 0, x = 0, y = 0; i < creature_count; i++, x+=830, y+=830) {
+    for (int i = 0; i < creature_count; i++) {
         int creature_id;
         int color;
         int type;
         cin >> creature_id >> color >> type; cin.ignore();
-        if (creature_id != -1)
-            fshs[creature_id] = {x, y};
+        if (type != -1)
+           fshs.insert(creature_id);
+        else
+            enemies.insert(creature_id);
     }
-
     // game loop
-    int flg = 1000;
+    int l = 0;
     while (1) {
-        int l0=1,l1=1;
-        vector<int> indxs;
+        int  emergency1=0,emergency2=0;
         int my_score;
         cin >> my_score; cin.ignore();
         int foe_score;
@@ -80,7 +69,6 @@ int main()
         for (int i = 0; i < my_scan_count; i++) {
             int creature_id;
             cin >> creature_id; cin.ignore();
-            cerr << "mySCAN " << creature_id << endl;
         }
         int foe_scan_count;
         cin >> foe_scan_count; cin.ignore();
@@ -97,13 +85,24 @@ int main()
             int emergency;
             int battery;
             cin >> drone_id >> drone_x >> drone_y >> emergency >> battery; cin.ignore();
+            cerr << "EMERGENCY " << emergency << endl;
             if (i) {
+                if(emergency) {
+                    cerr << "WTF2\n";
+                    for (auto &h:fishesScaned2) cerr << "FISHES SCANED2 " << h << endl;
+                    fshs.insert(fishesScaned2.begin(), fishesScaned2.end());
+                    fishesScaned2.clear();
+                }
                 myID2 = drone_id, myX2 = drone_x, myY2 = drone_y;
-                // if (emergency) l1 = 0;
             }
             else {
+                if(emergency) {
+                    cerr << "WTF1\n";
+                    for (auto &h:fishesScaned1) cerr << "FISHES SCANED1 " << h << endl;
+                    fshs.insert(fishesScaned1.begin(), fishesScaned1.end());
+                    fishesScaned1.clear();
+                }
                 myID1 = drone_id, myX1 = drone_x, myY1 = drone_y;
-                // if (emergency) l0 = 0;
             }
         }
         int foe_drone_count;
@@ -123,9 +122,11 @@ int main()
             int drone_id;
             int creature_id;
             cin >> drone_id >> creature_id; cin.ignore();
-            if ((drone_id == myID1 || drone_id == myID2) && fshs.count(creature_id))
+            if ((drone_id == myID1 || drone_id == myID2) && fshs.count(creature_id)) {
+                if (drone_id == myID1) fishesScaned1.insert(creature_id);
+                else fishesScaned2.insert(creature_id);
                 fshs.erase(creature_id), cerr << "drone scan count " << creature_id << endl;
-            indxs.push_back(creature_id);
+            }
         }
         int visible_creature_count;
         cin >> visible_creature_count; cin.ignore();
@@ -136,31 +137,34 @@ int main()
             int creature_vx;
             int creature_vy;
             cin >> creature_id >> creature_x >> creature_y >> creature_vx >> creature_vy; cin.ignore();
-            cerr << "VISIBLE " << creature_id << endl;
-            // if (creature_id == -1) {
-            //     emergen = 1;
-            // }
+            if (enemies.count(creature_id)) emergency1=1, emergency2=1;
         }
         int radar_blip_count;
         cin >> radar_blip_count; cin.ignore();
         vector<string> rdrs1, rdrs2;
+        multiset<pair<int,int>> st;
         for (int i = 0; i < radar_blip_count; i++) {
             int drone_id;
             int creature_id;
             string radar;
             cin >> drone_id >> creature_id >> radar; cin.ignore();
-            if (creature_id != -1 && (drone_id == myID1 || drone_id == myID2) && fshs.count(creature_id)) {
+            if (enemies.count(creature_id)) continue;
+            if ((drone_id == myID1 || drone_id == myID2) && fshs.count(creature_id)) {
                 if (drone_id == myID1)
                     rdrs1.push_back(radar);
                 else
                     rdrs2.push_back(radar);
             }
         }
-        displayMove(rdrs1, myX1, myY1, l0);
-        // displayMove(rdrs2, myX2, myY2, l1);
-        cout << "MOVE " << myX2 << " " << flg << " 0\n";
-        if (myY2 == 1000) flg = 0;
-        else if (myY2 == 0)flg = 1000;
-
+        if (emergency1)
+            displayMove(rdrs1, myX1, myY1, 0);
+        else
+            displayMove(rdrs1, myX1, myY1, l);
+        if (emergency2)
+            displayMove(rdrs2, myX2, myY2, 0);
+        else
+            displayMove(rdrs2, myX2, myY2, l);
+        l = !l;
+        for (auto &i:fshs) cerr << "FISHES " << i << endl;
     }
 }
